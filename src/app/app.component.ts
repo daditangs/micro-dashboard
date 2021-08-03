2739362722a21286485f9f658b590dbf3c53ecbd
Betsy <bethsedea@gmail.com>
2021-08-04 00:13:44 +0800

    create sample dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { assetUrl } from 'src/single-spa/asset-url';
+
 
 @Component({
   selector: 'app2-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app2';
+
+  martrust = assetUrl("logo_martrust_black.png");
 }
